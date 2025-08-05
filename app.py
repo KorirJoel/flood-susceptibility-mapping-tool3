90c99a4ed06d9d253b1e9fd54199f7930191581f
KorirJoel <163920804+KorirJoel@users.noreply.github.com>
2025-08-05 13:20:28 +0300

    app.py

--- app.py
+++ app.py
@@ -204,9 +204,11 @@ def predict():
         # Return any errors that occur
         return jsonify({'error': str(e)}), 500
 
-
-# Run the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
+
 
 
